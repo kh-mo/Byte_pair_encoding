0a77d7a5899fda4698c89e94a1455e6b6b8ef373
kh-mo <momkdh2@naver.com>
2019-03-24 01:27:39 +0900

    init

--- learn_bpe.py
+++ learn_bpe.py
@@ -1,11 +1,13 @@
-# python learn_bpe.py -input=D:/mt_data/korean-english-park.train.ko -output=D:/mt_data/korean-english-park.train.ko.bpe_result
+# python learn_bpe.py -input=D:/mt_data/korean-english-park.train.ko -output=D:/mt_data/korean-english-park.train.ko.bpe_result -symbol_count=2000
 
+import re
 import argparse
 from collections import Counter, defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-input', help="input path")
 parser.add_argument('-output', help="output path")
+parser.add_argument('-symbol_count', type=int, help="a_number_of_new_symbol")
 args = parser.parse_args()
 
 def get_vocabulary(data):
@@ -13,20 +15,59 @@ def get_vocabulary(data):
     vocab = Counter()
     for i, line in enumerate(data):
         for word in line.strip('\n').split(' '):
-            vocab[word] += 1
+            if word:
+                vocab[word] += 1
+    vocab = [(tuple(word), count) for (word, count) in vocab.items()]
     return vocab
 
-def learn_bpe(infile, outfile):
+def get_pair_statistics(data):
+    '''
+    stats : (pair, 빈도수) 반환
+    index_inform : vocab에서 (pair : voca내 index) 반환
+    '''
+    stats = Counter()
+    index_inform = defaultdict(list)
+    for idx, (word, count) in enumerate(data):
+        first_char = word[0]
+        for second_char in word[1:]:
+            stats[first_char, second_char] += count
+            index_inform[first_char, second_char].append(idx)
+            first_char = second_char
+    return stats, index_inform
+
+def replace_pair(pair, vocab, indices):
+    """('A', 'B') pair를 'AB'로 변환하여 새로운 voca 반환"""
+    first, second = pair
+    pair_str = ''.join(pair)
+    pattern = re.compile(r'(?<!\S)' + re.escape(first + ' ' + second) + r'(?!\S)')
+    for j in indices[pair]:
+        word, freq = vocab[j]
+        new_word = ' '.join(word)
+        new_word = pattern.sub(pair_str, new_word)
+        new_word = tuple(new_word.split(' '))
+        vocab[j] = (new_word, freq)
+    return vocab
+
+def learn_bpe(infile, outfile, count):
+    '''bpe 학습'''
     outfile.write('# bpe version 0.1 by kh-mo')
     vocab = get_vocabulary(infile)
-    vocab = dict([(tuple(word), count) for (word, count) in vocab.items()]) ### get_vocabulary에 녹아있으면 안되는 이유?
-    sorted_vocab = sorted(vocab.items(), key=lambda x:x[1], reverse=True)
 
-    stats, indices = get_pair_statistics(sorted_vocab)
-    most_frequent = max(stats, key=lambda x: (stats[x], x))
-    outfile.write('{0} {1}\n'.format(*most_frequent))
+    for i in range(count):
+        stats, index_inform = get_pair_statistics(vocab)
+        most_frequent = max(stats.items(), key=lambda x: x[1])[0]
+        outfile.write('{0} {1}\n'.format(*most_frequent))
+        vocab = replace_pair(most_frequent, vocab, index_inform)
+        if i % 100 == 0:
+            print("count :", i)
 
 if __name__ == "__main__":
     input = open(args.input, 'r', encoding='utf8').readlines()
     with open(args.output, 'w', encoding='utf8') as out:
-        learn_bpe(input, out)
+        learn_bpe(input, out, args.symbol_count)
+
+#
+# input = open("D:/mt_data/korean-english-park.train.ko", 'r', encoding='utf8').readlines()
+# infile = input
+# data = vocab
+# data = sorted_vocab
